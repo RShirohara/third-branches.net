e07bea6beed4d6d833e78a5baeffee2a715595d7
Ray Shirohara <RShirohara@proton.me>
2025-10-31 21:04:27 +0900

    chore(deps): bump container dependency cloudflare/cloudflared to 2025.10.1 (#32)
    Bump container dependencycloudflare/cloudflaredto 2025.10.1.
    Release Note: <https://github.com/cloudflare/cloudflared/releases/tag/2025.10.1>
    
    And downgrade AWS Lightsail container dependencysuperseriousbusiness/gotosocialto 0.20.0.
    Ref: <https://codeberg.org/superseriousbusiness/gotosocial/issues/4527>

--- services/ap.third-branches.net/main.tf
+++ services/ap.third-branches.net/main.tf
@@ -52,7 +52,7 @@ resource "aws_lightsail_container_service_deployment_version" "gotosocial" {
 
   container {
     container_name = "app"
-    image = "superseriousbusiness/gotosocial:0.20.1"
+    image = "superseriousbusiness/gotosocial:0.20.0"
     environment = {
       SERVICE_CON = "service://localhost"
       TZ = "Asia/Tokyo"
@@ -76,7 +76,7 @@ resource "aws_lightsail_container_service_deployment_version" "gotosocial" {
   }
   container {
     container_name = "tunnel"
-    image = "cloudflare/cloudflared:2025.10.0"
+    image = "cloudflare/cloudflared:2025.10.1"
     command = ["tunnel", "run"]
     environment = {
       SERVICE_CON = "service://localhost"
