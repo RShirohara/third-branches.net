0c2d9e37c7b2aeecaceb3eda2a78330f939091b3
Ray Shirohara <RShirohara@proton.me>
2026-02-27 20:44:04 +0900

    chore(deps): bump container dependency `superseriousbusiness/gotosocial` to `0.21.0`
    Bump container dependency `superseriousbusiness/gotosocial` to `0.21.0`.
    Release Note: <https://codeberg.org/superseriousbusiness/gotosocial/releases/tag/v0.21.0>

--- services/ap.third-branches.net/main.tf
+++ services/ap.third-branches.net/main.tf
@@ -52,7 +52,7 @@ resource "aws_lightsail_container_service_deployment_version" "gotosocial" {
 
   container {
     container_name = "app"
-    image = "superseriousbusiness/gotosocial:0.20.3"
+    image = "superseriousbusiness/gotosocial:0.21.0"
     environment = {
       SERVICE_CON = "service://localhost"
       TZ = "Asia/Tokyo"
